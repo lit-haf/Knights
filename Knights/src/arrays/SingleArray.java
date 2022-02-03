1335a73d23cfc1e898032e5d7b9fa22f1fdae8c3
ADMIN <ADMIN@DESKTOP-QA8ESVI>
2022-02-04 00:41:56 +0530

    array

---
+++ Knights/src/arrays/SingleArray.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class SingleArray {
+
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4,5};
+		arr[4]=10;
+		int ar[]=new int[5];
+		ar[1]= 6;
+		
+		System.out.println(arr.length);
+		System.out.println(arr[4]);
+		System.out.println(ar[1]);
+
+	}
+
+}
