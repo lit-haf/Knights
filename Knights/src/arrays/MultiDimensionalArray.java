638e37e26c0320dbcd91eddd79dd5a3d6fa4f854
ADMIN <ADMIN@DESKTOP-QA8ESVI>
2022-02-04 00:51:41 +0530

    array

--- Knights/src/arrays/MultiDimensionalArray.java
+++ Knights/src/arrays/MultiDimensionalArray.java
@@ -3,8 +3,18 @@ package arrays;
 public class MultiDimensionalArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int arr[][]= { 
+				
+				{1,2,3,4,5},
+				{ 6,7,8,9,10},
+				{7336,73736,938447,83732}
+		};
+		System.out.println(arr.length);
+		System.out.println(arr[2].length);
+		
+		int ar[][]=new int[3][5];
+		ar[0][3]=10;
+		System.out.println(ar[0][3]);
 	}
 
 }
